cc6db4e015ea681f36e43bdaf3d70232be6895e9
Janine Walther <walj@zhaw.ch>
2016-06-30 10:46:29 +0200

    tst

---
+++ src/ch/zhaw/walj/projectmanagement/Employee.java
@@ -0,0 +1,9 @@
+package ch.zhaw.walj.projectmanagement;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/newEmployee")
+public class Employee extends HttpServlet{
+	
+}
