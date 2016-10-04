9af6ea1289b846d9c5c6dadcb7e7e4e8d3b9c451
Janine Walther <walj@zhaw.ch>
2016-10-04 16:54:52 +0200

    sql files added

--- SQL/projectmanagement_structure_only.sql
+++ SQL/projectmanagement_structure_only.sql
@@ -37,29 +37,6 @@ CREATE TABLE IF NOT EXISTS `Assignments` (
   KEY `EmployeeIDFS` (`EmployeeIDFS`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=45 ;
 
---
--- Dumping data for table `Assignments`
---
-
-INSERT INTO `Assignments` (`AssignmentID`, `TaskIDFS`, `EmployeeIDFS`) VALUES
-(28, 18, 1),
-(29, 19, 1),
-(30, 20, 1),
-(31, 21, 1),
-(32, 22, 1),
-(33, 18, 1),
-(34, 19, 1),
-(35, 20, 1),
-(36, 21, 1),
-(37, 22, 1),
-(38, 18, 7),
-(39, 19, 7),
-(40, 20, 7),
-(41, 21, 7),
-(42, 22, 7),
-(43, 21, 2),
-(44, 21, 2);
-
 -- --------------------------------------------------------
 
 --
@@ -74,14 +51,6 @@ CREATE TABLE IF NOT EXISTS `Bookings` (
   PRIMARY KEY (`BookingID`),
   KEY `AssignmentID` (`AssignmentIDFS`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `Bookings`
---
-
-INSERT INTO `Bookings` (`BookingID`, `AssignmentIDFS`, `Month`, `Hours`) VALUES
-(1, 28, 1, 103),
-(2, 38, 1, 177);
 
 -- --------------------------------------------------------
 
@@ -103,16 +72,6 @@ CREATE TABLE IF NOT EXISTS `Employees` (
   KEY `Supervisor` (`Supervisor`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
 
---
--- Dumping data for table `Employees`
---
-
-INSERT INTO `Employees` (`EmployeeID`, `Firstname`, `Lastname`, `Kuerzel`, `Password`, `Mail`, `Supervisor`) VALUES
-(1, 'Janine', 'Walther', 'walj', 'cojt0Pw//L6ToM8G41aOKFIWh7w=', 'walj@zhaw.ch', NULL),
-(2, 'Test', 'User', 'test', 'cojt0Pw//L6ToM8G41aOKFIWh7w=', 'test@test.ch', 1),
-(7, 'Sami', 'Klaus', 'sami', 'cojt0Pw//L6ToM8G41aOKFIWh7w=', 'sami@whatever.com', 1),
-(8, 'Panda', 'BÃ¤r', 'pand', 'cojt0Pw//L6ToM8G41aOKFIWh7w=', 'panda.baer@pandasareawesome.com', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -131,15 +90,6 @@ CREATE TABLE IF NOT EXISTS `Expenses` (
   KEY `EmployeeIDFS` (`EmployeeIDFS`),
   KEY `ProjectIDFS` (`ProjectIDFS`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
-
---
--- Dumping data for table `Expenses`
---
-
-INSERT INTO `Expenses` (`ExpenseID`, `ProjectIDFS`, `EmployeeIDFS`, `Costs`, `Type`, `Description`, `Date`) VALUES
-(1, 8, 1, 900, 'Travel', 'Flight to secret destination', '2013-09-20'),
-(2, 8, 1, 280, 'Overnight Stay', 'Hotel Oceanview 2 Nights inkl. meals', '2013-09-20'),
-(4, 8, 1, 900, 'Travel', 'Back from secret destination', '2015-09-20');
 
 -- --------------------------------------------------------
 
@@ -162,16 +112,6 @@ CREATE TABLE IF NOT EXISTS `Projects` (
   KEY `ProjectLeader` (`ProjectLeader`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=12 ;
 
---
--- Dumping data for table `Projects`
---
-
-INSERT INTO `Projects` (`ProjectIDFS`, `ProjectShortname`, `ProjectName`, `ProjectLeader`, `TotalBudget`, `Currency`, `ProjectStart`, `ProjectEnd`, `Partner`, `Archive`) VALUES
-(8, 'IAPNH I', 'Insert awesome project name here', 1, 300000, 'CHF', '2016-09-01', '2018-08-31', 'ABCD, CDEF', 0),
-(9, 'IAPNH II', 'Insert awesome project name here', 1, 300000, 'CHF', '2016-01-01', '2018-01-31', 'ABCD, CDEF', 0),
-(10, 'IAPNH III', 'Insert awesome project name here', 2, 300000, 'CHF', '2016-01-01', '2018-01-31', 'ABCD, CDEF', 0),
-(11, 'IAPNH IV', 'Insert awesome project name here', 7, 300000, 'CHF', '2016-01-01', '2018-01-31', 'ABCD, CDEF', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -190,32 +130,6 @@ CREATE TABLE IF NOT EXISTS `Tasks` (
   KEY `WorkpackageIDFS` (`WorkpackageIDFS`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=38 ;
 
---
--- Dumping data for table `Tasks`
---
-
-INSERT INTO `Tasks` (`TaskID`, `WorkpackageIDFS`, `TaskName`, `TaskStart`, `TaskEnd`, `PMs`, `Budget`) VALUES
-(18, 16, 'Cool Task I', '2016-09-01', '2016-12-31', 8, 40500),
-(19, 16, 'Cool Task II', '2017-01-01', '2017-01-31', 2, 10100),
-(20, 17, 'Awesome Task', '2017-02-01', '2017-10-31', 20, 101000),
-(21, 18, 'Cool and Awesome Task', '2017-11-01', '2018-03-31', 10, 50500),
-(22, 19, 'Ultra Awesome and Cool', '2018-04-01', '2018-08-31', 10, 50500),
-(23, 20, 'Cool Task I', '2016-01-01', '2016-01-31', 8, 40500),
-(24, 20, 'Cool Task II', '2017-01-01', '2017-01-31', 2, 10100),
-(25, 21, 'Awesome Task', '2017-01-01', '2017-01-31', 20, 101000),
-(26, 22, 'Cool and Awesome Task', '2017-01-01', '2018-01-31', 10, 50500),
-(27, 23, 'Ultra Awesome and Cool', '2018-01-01', '2018-01-31', 10, 50500),
-(28, 24, 'Cool Task I', '2016-01-01', '2016-01-31', 8, 40500),
-(29, 24, 'Cool Task II', '2017-01-01', '2017-01-31', 2, 10100),
-(30, 25, 'Awesome Task', '2017-01-01', '2017-01-31', 20, 101000),
-(31, 26, 'Cool and Awesome Task', '2017-01-01', '2018-01-31', 10, 50500),
-(32, 27, 'Ultra Awesome and Cool', '2018-01-01', '2018-01-31', 10, 50500),
-(33, 28, 'Cool Task I', '2016-01-01', '2016-01-31', 8, 40500),
-(34, 28, 'Cool Task II', '2017-01-01', '2017-01-31', 2, 10100),
-(35, 29, 'Awesome Task', '2017-01-01', '2017-01-31', 20, 101000),
-(36, 30, 'Cool and Awesome Task', '2017-01-01', '2018-01-31', 10, 50500),
-(37, 31, 'Ultra Awesome and Cool', '2018-01-01', '2018-01-31', 10, 50500);
-
 -- --------------------------------------------------------
 
 --
@@ -230,17 +144,6 @@ CREATE TABLE IF NOT EXISTS `Wage` (
   PRIMARY KEY (`WageID`),
   KEY `EmployeeIDFS` (`EmployeeIDFS`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=10 ;
-
---
--- Dumping data for table `Wage`
---
-
-INSERT INTO `Wage` (`WageID`, `EmployeeIDFS`, `WagePerHour`, `ValidFrom`) VALUES
-(1, 1, 30, '2015-06-01'),
-(2, 1, 30, '2015-06-26'),
-(3, 2, 30, '2015-06-01'),
-(5, 7, 60, '2015-06-01'),
-(6, 8, 30, '2016-10-03');
 
 -- --------------------------------------------------------
 
@@ -257,28 +160,6 @@ CREATE TABLE IF NOT EXISTS `Workpackages` (
   PRIMARY KEY (`WorkpackageID`),
   KEY `ProjectIDFS` (`ProjectIDFS`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=32 ;
-
---
--- Dumping data for table `Workpackages`
---
-
-INSERT INTO `Workpackages` (`WorkpackageID`, `ProjectIDFS`, `WPName`, `WPStart`, `WPEnd`) VALUES
-(16, 8, 'Cool Workpackage', '2016-09-01', '2017-01-31'),
-(17, 8, 'Awesome Workpackage', '2017-02-01', '2017-10-31'),
-(18, 8, 'Cool and Awesome Workpackage', '2017-11-01', '2018-03-31'),
-(19, 8, 'Ultra Awesome and Cool Workpackage', '2018-04-01', '2018-08-31'),
-(20, 9, 'Cool Workpackage', '2016-01-01', '2017-01-31'),
-(21, 9, 'Awesome Workpackage', '2017-01-01', '2017-01-31'),
-(22, 9, 'Cool and Awesome Workpackage', '2017-01-01', '2018-01-31'),
-(23, 9, 'Ultra Awesome and Cool Workpackage', '2018-01-01', '2018-01-31'),
-(24, 10, 'Cool Workpackage', '2016-01-01', '2017-01-31'),
-(25, 10, 'Awesome Workpackage', '2017-01-01', '2017-01-31'),
-(26, 10, 'Cool and Awesome Workpackage', '2017-01-01', '2018-01-31'),
-(27, 10, 'Ultra Awesome and Cool Workpackage', '2018-01-01', '2018-01-31'),
-(28, 11, 'Cool Workpackage', '2016-01-01', '2017-01-31'),
-(29, 11, 'Awesome Workpackage', '2017-01-01', '2017-01-31'),
-(30, 11, 'Cool and Awesome Workpackage', '2017-01-01', '2018-01-31'),
-(31, 11, 'Ultra Awesome and Cool Workpackage', '2018-01-01', '2018-01-31');
 
 --
 -- Constraints for dumped tables
